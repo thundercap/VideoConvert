e3d8704a0c7c9e7ba79b9ed51e0be3ac688de458
silicon <59467328+thundercap@users.noreply.github.com>
2026-02-16 11:24:56 +0530

    Add S3 access policy for MediaConvert

--- mediaconvert.tf
+++ mediaconvert.tf
@@ -13,7 +13,31 @@ resource "aws_iam_role" "mediaconvert_role" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "mediaconvert_s3_access" {
+resource "aws_iam_policy" "mediaconvert_s3_policy" {
+  name = "mediaconvert-s3-access"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject"
+        ]
+        Resource = "${aws_s3_bucket.video_bucket.arn}/uploads/*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:PutObject"
+        ]
+        Resource = "${aws_s3_bucket.video_bucket.arn}/processed/*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "mediaconvert_custom_attach" {
   role       = aws_iam_role.mediaconvert_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+  policy_arn = aws_iam_policy.mediaconvert_s3_policy.arn
 }
