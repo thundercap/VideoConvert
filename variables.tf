def8c2e1ee882a2313f8059020d374d24f8795c9
silicon <59467328+thundercap@users.noreply.github.com>
2026-02-16 11:13:43 +0530

    Add Terraform variables for region and S3 bucket

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "region" {
+  default = "ap-south-1"
+}
+
+variable "bucket_name" {
+  description = "Unique S3 bucket name"
+  type        = string
+}
+
+variable "lambda_function_name" {
+  default = "video-transcode-lambda"
+}
