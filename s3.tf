57fd6ce55c59320647b4f85541834eb803a9bdce
silicon <59467328+thundercap@users.noreply.github.com>
2026-02-16 11:17:23 +0530

    Add versioning and encryption to S3 bucket
    Added versioning and server-side encryption configurations to the S3 bucket.

--- s3.tf
+++ s3.tf
@@ -2,16 +2,20 @@ resource "aws_s3_bucket" "video_bucket" {
   bucket = var.bucket_name
 }
 
-resource "aws_s3_bucket_notification" "bucket_notification" {
+resource "aws_s3_bucket_versioning" "versioning" {
   bucket = aws_s3_bucket.video_bucket.id
 
-  lambda_function {
-    lambda_function_arn = aws_lambda_function.video_lambda.arn
-    events              = ["s3:ObjectCreated:*"]
-    filter_prefix       = "uploads/"
+  versioning_configuration {
+    status = "Enabled"
   }
+}
 
-  depends_on = [
-    aws_lambda_permission.allow_s3
-  ]
+resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
+  bucket = aws_s3_bucket.video_bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
 }
